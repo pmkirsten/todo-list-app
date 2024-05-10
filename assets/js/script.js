4074abfcffc0cb6b9560dd8156ef59dd140a715f
Pablo Martínez Kirsten <pablo.martinez@imatia.com>
2024-05-10 11:09:37 +0200

    Refactored code. Add task button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,13 @@
 var btn_add_task = document.getElementById("add-task-btn");
+var input = document.getElementById("add-task-input");
+var toDoContainer = document.getElementById("to-do-container");
+btn_add_task.addEventListener('click', addTask);
+
+function addTask() {
+    let taskDiv = document.createElement("div");
+    taskDiv.className = "task";
+    taskDiv.innerText = input.value;
+    input.value = '';
+    toDoContainer.appendChild(taskDiv)
+}
+
