43b6192f5d1aab53561c16586be075505415b4fa
Pablo Martínez Kirsten <pablo.martinez@imatia.com>
2024-05-10 12:27:30 +0200

    Update App

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,7 @@ function addTask() {
     taskDiv.className = "task";
     taskDiv.appendChild(createMarkAsDoneIcon());
     taskDiv.appendChild(createTaskName(input.value));
+    taskDiv.appendChild(createTrashIcon());
     input.value = '';
     toDoContainer.appendChild(taskDiv)
 }
@@ -23,5 +24,12 @@ function createTaskName(taskName) {
     let span = document.createElement("span");
     span.className = "task_name"
     span.innerText = taskName;
+    return span;
+}
+
+function createTrashIcon() {
+    let span = document.createElement("span");
+    span.className = "material-symbols-outlined trash"
+    span.innerText = "delete";
     return span;
 }
