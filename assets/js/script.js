4bf4909dec7093254686e98ebcae3483e137f82a
Pablo Martínez Kirsten <pablo.martinez@imatia.com>
2024-05-10 13:45:59 +0200

    Update app

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,24 @@
 var btnAddtask = document.getElementById("add-task-btn");
 var input = document.getElementById("add-task-input");
 var toDoContainer = document.getElementById("to-do-container");
+var doneContainer = document.getElementById("done-container");
 btnAddtask.addEventListener('click', addTask);
 
 function addTask() {
     let taskDiv = document.createElement("div");
     taskDiv.className = "task";
-    taskDiv.appendChild(createMarkAsDoneIcon());
+    let mark = createMarkAsDoneIcon()
+    taskDiv.appendChild(mark);
+    mark.addEventListener('click', function () {
+        this.classList.toggle("hidden")
+        doneContainer.appendChild(this.parentNode);
+    })
+    let unmark = createMarkAsToDoIcon()
+    taskDiv.appendChild(unmark);
+    unmark.addEventListener('click', function () {
+        this.classList.toggle("hidden")
+        toDoContainer.appendChild(this.parentNode);
+    })
     taskDiv.appendChild(createTaskName(input.value));
     taskDiv.appendChild(createTrashIcon());
     input.value = '';
@@ -17,6 +29,13 @@ function createMarkAsDoneIcon() {
     let span = document.createElement("span");
     span.className = "material-symbols-outlined mark-as-done"
     span.innerText = "radio_button_unchecked";
+    return span
+}
+
+function createMarkAsToDoIcon() {
+    let span = document.createElement("span");
+    span.className = "material-symbols-outlined mark-as-to-do hidden"
+    span.innerText = "check_circle";
     return span
 }
 
